875fdaff3ff62e2fc406a1ff4522155bedf4577d
Mounika kuncha <114674603+mounikakuncha14@users.noreply.github.com>
2023-08-07 18:39:26 +0530

    Add files via upload

---
+++ 03 chatgpt chat assistant website.py
@@ -0,0 +1,20 @@
+import openai
+import gradio
+
+openai.api_key = "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"
+
+messages = [{"role": "system", "content": "you are a senior professor who knows every subject"}]
+
+def CustomChatGPT(user_input):
+    messages.append({"role": "user", "content": user_input})
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = messages
+    )
+    ChatGPT_reply = response["choices"][0]["message"]["content"]
+    messages.append({"role": "assistant", "content": ChatGPT_reply})
+    return ChatGPT_reply
+
+demo = gradio.Interface(fn=CustomChatGPT, inputs = "text", outputs = "text", title = "A Chat Bot By AI")
+
+demo.launch(share=True)
